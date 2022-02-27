c7c92dc0fb9840ff9956a5df10d7538c5db0bc38
Robert Kaussow <mail@thegeeklab.de>
2022-02-27 17:56:08 +0100

    chore: add unit tests for ip address gathering (#157)

--- prometheuspvesd/test/unit/test_discovery.py
+++ prometheuspvesd/test/unit/test_discovery.py
@@ -1,22 +1,20 @@
 """Test Autostop class."""
 
-import pytest
-from proxmoxer import ProxmoxAPI
-
-from prometheuspvesd import discovery
-
 pytest_plugins = [
     "prometheuspvesd.test.fixtures.fixtures",
 ]
 
 
-@pytest.fixture
-def discovery_fixture(mocker):
-    mocker.patch.object(
-        discovery.Discovery, "_auth", return_value=mocker.create_autospec(ProxmoxAPI)
-    )
+def get_mock(*args):
+    networks = args[0]
+    args = args[1:]
 
-    return discovery.Discovery()
+    if "info" in args:
+        return True
+    if "network-get-interfaces" in args:
+        return {"result": networks}
+
+    return False
 
 
 def test_exclude(discovery_fixture, qemus):
@@ -26,3 +24,26 @@ def test_exclude(discovery_fixture, qemus):
     filtered = discovery_fixture._exclude(qemus)
 
     assert filtered == expected
+
+
+def test_validate_ip(discovery_fixture, addresses):
+    # IPv4 validation
+    for address in addresses["ipv4_valid"]:
+        assert discovery_fixture._validate_ip(address)
+    for address in addresses["ipv4_invalid"]:
+        assert not discovery_fixture._validate_ip(address)
+
+    # IPv6 validation
+    for address in addresses["ipv6_valid"]:
+        assert discovery_fixture._validate_ip(address)
+    for address in addresses["ipv6_invalid"]:
+        assert not discovery_fixture._validate_ip(address)
+
+
+def test_get_ip_addresses(mocker, discovery_fixture, networks):
+    discovery_fixture.client.get.side_effect = lambda *args: get_mock(networks, *args)
+
+    assert discovery_fixture._get_ip_addresses("qemu", "dummy", "dummy") == (
+        networks[1]["ip-addresses"][0]["ip-address"],
+        networks[1]["ip-addresses"][2]["ip-address"],
+    )
